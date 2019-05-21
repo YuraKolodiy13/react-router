f3b4a93a35f4e4b797f402838cb459abe9e93b7e
YuraKolodiy13 <Timberwolves13>
2019-05-21 10:47:10 +0300

    work more

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react'
 import './App.scss'
-import {Route, NavLink, Switch} from  'react-router-dom'
+import {Route, NavLink, Switch, Redirect} from  'react-router-dom'
 import About from './About/About'
 import Cars from './Cars/Cars'
 import CarDetail from "./CarDetail/CarDetail";
 
 class App extends Component {
+
+  state = {
+    isLoggedIn : false
+  }
+
   render() {
 
     return (
@@ -28,12 +33,21 @@ class App extends Component {
 
         <hr/>
 
-        <Switch>
-        <Route path="/" exact render={() => <h1>home page</h1>} />
+        <div style={{textAlign: 'center'}}>
+          <button onClick={() => this.setState({isLoggedIn: !this.state.isLoggedIn})}>login</button>
+          <p>{this.state.isLoggedIn ? 'true' : 'false'}</p>
+        </div>
 
-        <Route path="/about" component={About} />
-        <Route path="/cars/:name" component={CarDetail} />
-        <Route path="/cars" component={Cars} />
+        <hr/>
+
+        <Switch>
+          <Route path="/" exact render={() => <h1>home page</h1>} />
+          {this.state.isLoggedIn ? <Route path="/about" component={About} /> : null}
+          {/*<Route path="/about" component={About} />*/}
+          <Route path="/cars/:name" component={CarDetail} />
+          <Route path="/cars" component={Cars} />
+          <Redirect to={'/'}/>
+          {/*<Route render={() => <h1 style={{color: 'red', textAlign: 'center'}}>404 not found</h1>}/>d*/}
         </Switch>
 
       </div>
